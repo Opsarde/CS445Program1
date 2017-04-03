615f063b091611839f0f948a9408110cc66b5f66
Opsarde <Opsarde@users.noreply.github.com>
2017-04-03 14:06:32 -0700

    DataReader class has line condition implemented

--- src/cs445program1/DataReader.java
+++ src/cs445program1/DataReader.java
@@ -4,12 +4,45 @@
  * and open the template in the editor.
  */
 package cs445program1;
+import java.util.ArrayList;
+import java.io.*;
 
 /**
  * This class reads data from a txt file and stores primitive shapes
  * in fields.  I am not sure if shapes are going to be polymorphic.
- * @author shun7817
+ * 
+ * @author Shun Lu
  */
 public class DataReader {
-    
+    private ArrayList<Shape> list;
+
+    /**
+     * This constructor constructs data read from txt
+     */
+    public DataReader(String filePath) {
+        list = new ArrayList<>(); 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line; 
+            while ( (line = reader.readLine()) != null) {
+                String[] data = line.split(" ");
+                if (data[0].equals("l")) {
+                    Point p1 = new Point(data[1].split(","));
+                    Point p2 = new Point(data[2].split(","));
+                    list.add(new Line(p1, p2));
+                }
+                // TODO: condition for ellipse and circle
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * METHOD: getData
+     * PURPOSE: get list from DataReader object
+     */
+    public ArrayList<Shape> getData() {
+        return list;
+    }
 }
