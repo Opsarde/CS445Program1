62eef488791473e0872e269cfc1c9961e329dacc
Opsarde <Opsarde@users.noreply.github.com>
2017-04-04 14:10:33 -0700

    Ellipse class implemented

--- src/cs445program1/Ellipse.java
+++ src/cs445program1/Ellipse.java
@@ -5,15 +5,43 @@
  */
 package cs445program1;
 
+import static org.lwjgl.opengl.GL11.*;
+
 /**
- *
- * @author shun7817
+ * Ellipse class: Has center, minor, and major axis that defines an ellipse
+ * 
+ * @author Shun Lu
  */
 public class Ellipse implements Shape{
 
+    private Point center;
+    private float rx, ry;
+
+    /**
+     * Constructor that one can pass in center, rx, and ry to create an ellipse
+     */
+    public Ellipse(Point center, float rx, float ry) {
+        this.center = center;
+        this.rx = rx;
+        this.ry = ry;
+    }
+
+    /**
+     * METHOD: draw
+     * PURPOSE: ellipse algorithm that draws points on projected matrix
+     */
     @Override
     public void draw() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        glColor3f(0.0f, 255.0f, 0.0f);
+        float theta = 0;
+        float x, y;
+        while (theta >= 0 && theta <= 2 * Math.PI) {
+            x = (float) (rx * Math.cos(theta));
+            y = (float) (ry * Math.sin(theta));
+            glBegin(GL_POINTS);
+                glVertex2f(center.x + x,center.y + y);
+            glEnd();
+            theta += (Math.PI / ((rx + ry) * 180.0f));
+        }
     }
-    
 }
