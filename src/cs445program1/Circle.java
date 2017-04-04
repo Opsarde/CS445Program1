b3cc0ef43b764b804363934ee94214925387f33e
Opsarde <Opsarde@users.noreply.github.com>
2017-04-04 14:10:10 -0700

    Circle class implemented

--- src/cs445program1/Circle.java
+++ src/cs445program1/Circle.java
@@ -5,15 +5,40 @@
  */
 package cs445program1;
 
+import static org.lwjgl.opengl.GL11.*;
+
 /**
- *
- * @author shun7817
+ * Circle Class: Has center and radius that define a circle
+ * @author Shun Lu
  */
 public class Circle implements Shape{
+    private Point center;
+    private float radius;
 
+    /**
+     * Constructor that one can pass in center and radius to create a circle
+     */
+    public Circle(Point center, float radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    /**
+     * METHOD: draw
+     * PURPOSE: circle algorithm that draws points on projected matrix
+     */
     @Override
     public void draw() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        glColor3f(0.0f, 0.0f, 255.0f);
+        float theta = 0;
+        float x, y;
+        while (theta >= 0 && theta <= 2 * Math.PI) {
+            x = (float) (radius * Math.cos(theta));
+            y = (float) (radius * Math.sin(theta));
+            glBegin(GL_POINTS);
+                glVertex2f(center.x + x,center.y + y);
+            glEnd();
+            theta += (Math.PI / (radius * 180.0f));
+        }
     }
-    
 }
